1b7ebf1b663944365c5b53d535842dd3550563b8
ppazosp <pablopazosp3@gmail.com>
2025-10-01 09:53:00 +0200

    v1.2.16 fix[frontend]: reduced file explorer number of columns on grid view

--- frontend/src/components/dashboard/FileExplorer.jsx
+++ frontend/src/components/dashboard/FileExplorer.jsx
@@ -264,7 +264,7 @@ const FileExplorer = ({
         </div>
 
         {/* Grid Layout */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
           {/* Folders First */}
           {folders_to_display.map((folder, index) => (
             <FolderIconItem
